e2d40f40bf126ec812cf61c5821a09c3a4fce109
Menk-bit <truongtueminh6390@gmail.com>
2025-12-25 21:48:29 +0700

    added pen and eraser

--- Lab04/GUIProject/hust/soict/dsai/javafx/PainterController.java
+++ Lab04/GUIProject/hust/soict/dsai/javafx/PainterController.java
@@ -3,9 +3,9 @@ package hust.soict.dsai.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 
@@ -14,6 +14,14 @@ public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+    
+    @FXML
+    private RadioButton penSelectionButton;
+    
+    @FXML
+    private RadioButton eraserSelectionButton;
+    
+    private int tool = 0;
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -22,9 +30,25 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-    	Circle newCircle = new Circle(event.getX(), 
-    			event.getY(), 4, Color.BLACK);
+    	Circle newCircle;
+    	
+    	if (tool == 1) {
+	    	newCircle = new Circle(event.getX(), 
+	    			event.getY(), 4, Color.BLACK);
+	    	
+    	} else {newCircle = new Circle(event.getX(), 
+    			event.getY(), 4, Color.WHITE);}
+    	
     	drawingAreaPane.getChildren().add(newCircle);
     }
-
+    
+    @FXML
+    void penSelected(ActionEvent event) {
+    	tool = 1;
+    }
+    
+    @FXML
+    void eraserSelected(ActionEvent event) {
+    	tool = 2;
+    }
 }
